ea582bbdc550284c4b2c6fdcecda4a5e06065fde
DWinst0n <daviwinstonsm@gmail.com>
2025-06-23 18:22:55 -0300

    Criação de arquivo para melhor divisão de funcionalidades, com acrécimo de verificação de expresões regulares e melhor vizualização no console

---
+++ src/ManipulaTexto.js
@@ -0,0 +1,45 @@
+function separarlinhas(texto) {
+	let linhas = texto.split("\n").filter((texto) => texto.trim().length > 0);
+	return linhas;
+}
+
+function limparPalavra(p) {
+	return p.replace(/[.,/#!$%&;:{}=\-_`~()]/g, "");
+}
+
+function verificarRedundancia(texto) {
+	const linhas = separarlinhas(texto);
+	console.log("Texto:");
+	console.log(linhas);
+	console.log(`Palavras repetidas em suas respectivas linhas:`);
+	linhas.forEach((p, i) => {
+		const resultado = {};
+		/* 	objeto[propriedade] = valor
+		 */
+		const palavras = p
+			.split(" ")
+			.map((p) => limparPalavra(p.toLowerCase()))
+			.filter((p) => p.length > 1);
+		palavras.forEach((palavra) => {
+			resultado[palavra] = (resultado[palavra] || 0) + 1;
+			/* Isso é o mesmo que:
+			if (!resultado[palavra]) {
+				resultado[palavra] = 0;
+			}
+			resultado[palavra] += 1; */
+		});
+		for (let palavra in resultado) {
+			if (resultado[palavra] < 2) {
+				delete resultado[palavra];
+			}
+		}
+		if (Object.keys(resultado).length < 1) {
+			return;
+		} else {
+			console.log(`linha ${i + 1} - "${linhas[i]}"`);
+			console.log(JSON.stringify(resultado, null, 2));
+		}
+	});
+}
+
+export default verificarRedundancia;

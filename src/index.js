40a35c1afbd3891de9ea55a1ed2cb445e9ce72fe
DWinst0n <daviwinstonsm@gmail.com>
2025-06-23 18:23:54 -0300

    Adição de tratamento de erros e múltiplos caminhos

--- src/index.js
+++ src/index.js
@@ -1,51 +1,37 @@
-const fs = require("fs");
+import verificarRedundancia from "./ManipulaTexto.js";
+import fs from "fs";
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
+const caminhosArquivos = process.argv.slice(2);
 
-fs.readFile(link, "utf-8", (erro, texto) => {
-	if (erro) {
-		throw erro;
-	}
-	verificarRedundancia(texto);
-});
-
-function separarlinhas(texto) {
-	let linhas = texto.split("\n").filter((texto) => texto.trim().length > 0);
-	return linhas;
-}
-
-function verificarRedundancia(texto) {
-	const linhas = separarlinhas(texto);
-	console.log("Texto:");
-	console.log(linhas);
-	console.log(`Palavras repetidas em suas respectivas linha:`);
-	linhas.forEach((p, i) => {
-		const resultado = {};
-		/* 	objeto[propriedade] = valor
-		 */
-		const palavras = p
-			.split(" ")
-			.map((p) => p.toLowerCase())
-			.filter((p) => p.length > 1);
-		palavras.forEach((palavra) => {
-			resultado[palavra] = (resultado[palavra] || 0) + 1;
-			/* Isso é o mesmo que:
-			if (!resultado[palavra]) {
-				resultado[palavra] = 0;
+caminhosArquivos.forEach((link) => {
+	fs.readFile(link, "utf-8", (erro, texto) => {
+		try {
+			if (erro) throw erro;
+			verificarRedundancia(texto);
+		} catch (erro) {
+			switch (erro.code) {
+				case "ENOENT":
+					console.log(`Arquivo não encontrado: ${link}`);
+					break;
+				case "EACCES":
+					console.log(`Permissão negada para acessar o arquivo: ${link}`);
+					break;
+				case "EISDIR":
+					console.log(`O caminho especificado é um diretório, não um arquivo: ${link}`);
+					break;
+				case "EMFILE":
+					console.log("Muitos arquivos abertos ao mesmo tempo. Tente fechar alguns arquivos.");
+					break;
+				case "ENAMETOOLONG":
+					console.log("O nome do arquivo ou caminho é muito longo.");
+					break;
+				case "EBADF":
+					console.log("Arquivo ou descritor de arquivo inválido.");
+					break;
+				default:
+					console.log("Algum erro aconteceu:", erro);
+					break;
 			}
-			resultado[palavra] += 1; */
-		});
-		for (let palavra in resultado) {
-			if (resultado[palavra] < 2) {
-				delete resultado[palavra];
-			}
-		}
-		if (Object.keys(resultado).length < 1) {
-			return;
-		} else {
-			console.log(`linha ${i} -`);
-			console.log(JSON.stringify(resultado, null, 2));
 		}
 	});
-}
+});
